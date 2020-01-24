5249f4e8c4bc5b80d1e24c6228f3dd9a7e38e747
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-24 09:50:51 -0800

    Add non number input prevention JS.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,27 @@ $(document).ready(function() {
     var inputNumber = parseInt($("input#numberInput").val());
    
     console.log(inputNumber)
+    
+    
+  if (isNaN(inputNumber)) {
+    alert("Please enter a number")
+} else
+    $("#result").show();
+
 
     for (var currentNumber = 0; currentNumber <= inputNumber; currentNumber += 1) {
       $("#result").append(currentNumber + " ");
      
       console.log(currentNumber)
-    
     };
     
-    console.log(currentNumber)
-  
+    
+    for (var index = 0; index < currentNumber.length; index += 1) {
+      if (currentNumber[index] === 1 || currentNumber[index] === 2 || currentNumber[index] === 3) {
+        currentNumber.splice(index, 1, "-");
+      };
+    };
+      console.log(currentNumber)
+    
   });
 });
