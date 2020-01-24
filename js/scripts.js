8e14a6ddbd3467c645697540b5c7b300732a69e2
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-24 12:35:53 -0800

    Change JS script organization.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,45 @@
+function numbersToList(inputNumber) {
+  var numberList = [];
+  for (var index = 0; index <= inputNumber; index += 1) {
+    numberList.push(index);
+  }
+  for (var index = 0; index <= inputNumber; index += 1) {
+    if (index.toString().includes("3")) {
+      numberList[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (index.toString().includes("2")) {
+      numberList[index] = "Boop!";
+    } else if (index.toString().includes("1")) {
+      numberList[index] = "Beep!";
+    }
+  }
+  return numberList;
+}
+
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
 // Gather user input
     var inputNumber = parseInt($("input#numberInput").val());
-    var outputNumber = numbersToResult(inputNumber)
+    var outputNumber = numbersToList(numberList)
+    
+
+    for (var currentNumber = 0; currentNumber <= numberList.length; currentNumber += 1) {
+      $("#result").prepend("<ul>" + "</ul>").append("<li>" + numberList + "</li>");
+      console.log(currentNumber)
+    };
+    
+
+
+
+
+
+
 // Refuse non number inputs
     if (isNaN(inputNumber)) {
-    alert("Please enter a number")
-    $("#result").hide();
+      alert("Please enter a number")
+      $("#result").hide();
     } else
-    $("#result").show();
-
-    function numbersToResult(number) {
-      var numberArray = [];
-      for (var index = 0; index <= number; index += 1) {
-        numbers.push(index);
-      }
-      for (var index = 0; index <= number; index += 1) {
-        if (index.toString().includes("3")) {
-          numbers[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
-        } else if (index.toString().includes("2")) {
-          numbers[index] = "Boop!";
-        } else if (index.toString().includes("1")) {
-          numbers[index] = "Beep!";
-        }
-      }
-      return numberArray;
-    }
-    
-
-    
-
-      for (var currentNumber = 0; currentNumber <= inputNumber; currentNumber += 1) {
-        $("#result").prepend("<ul>" + "</ul>").append("<li>" + currentNumber + "</li>");
-        console.log(currentNumber)
-      };
-
+      $("#result").show();
   });
 
 });
