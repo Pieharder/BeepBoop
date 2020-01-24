7586c80e680f5f3a64eb27936ca6a07eb8cca4b4
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-24 15:59:08 -0800

    Add CSS styling elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
+// User Interface (or front-end) logic:
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
-// Gather user input
+    
     var inputNumber = $("input#numberInput").val();
     var adjustedInput = numbersToList(inputNumber)
     
-    for (var index = 0; index <= adjustedInput.length; index += 1) {
-      $("#result").prepend("<ul>" + "</ul>").append("<li>" + adjustedInput + "</li>");
+    for (var index = 0; index < adjustedInput.length; index += 1) {
+      $("#result").prepend("<ul>" + "</ul>").append("<li>" + adjustedInput[index] + "</li>");
     };
-    
-    // Refuse non number inputs
+// Filters non numeric inputs:
     if (isNaN(inputNumber)) {
       alert("Please enter a number")
       $("#result").hide();
     } else
       $("#result").show();
     
-    $("#formReset").click(function () {
-      $("#result").hide();
+  $("#formReset").click(function () {
+    $("li").remove();
     });
   });
 });
     
 
-
+// Business (or back-end) logic:
 function numbersToList(inputNumber) {
 
   var inputArray = [];
   for (var index = 0; index <= inputNumber; index += 1) {
-    var incrString = index.toString();
-    if (incrString.includes("3")) {
+    var stringArray = index.toString();
+    if (stringArray.includes("3")) {
       inputArray.push("I'm sorry, Dave. I can't do that.");
-    } else if (incrString.includes("2")) {
+    } else if (stringArray.includes("2")) {
       inputArray.push("Boop!");
-    } else if (incrString.includes("1")) {
+    } else if (stringArray.includes("1")) {
       inputArray.push("Beep!");
     } else {
-      inputArray.push(incrString)
+      inputArray.push(stringArray)
     }
   }
   return inputArray
