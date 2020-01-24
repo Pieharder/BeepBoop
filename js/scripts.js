009566a3d6bec5a232bf49e8e22b9bf90bfecbaf
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-24 14:29:14 -0800

    Add complete functionality to JS.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,19 @@ $(document).ready(function() {
     event.preventDefault();
 // Gather user input
     var inputNumber = $("input#numberInput").val();
+    var adjustedInput = numbersToList(inputNumber)
+    
+    for (var index = 0; index <= adjustedInput.length; index += 1) {
+      $("#result").prepend("<ul>" + "</ul>").append("<li>" + adjustedInput + "</li>");
+    };
+
+    // Refuse non number inputs
+    if (isNaN(inputNumber)) {
+      alert("Please enter a number")
+      $("#result").hide();
+    } else
+      $("#result").show();
+    
 
   });
 });
@@ -14,37 +27,19 @@ $(document).ready(function() {
 
 
 function numbersToList(inputNumber) {
-  var finalList
+
+  var inputArray = [];
   for (var index = 0; index <= inputNumber; index += 1) {
-    if(containsNumber(index, "3")) {
-      numberList += "I'm sorry, Dave. I'm afraid I can't do that.";
-    } else if(containsNumber(index, "2")) {
-      numberList += "Boop!";
-    } else if(containsNumber(index, "1")) {
-      numberList += "Beep!";
+    var incrString = index.toString();
+    if (incrString.includes("3")) {
+      inputArray.push("I'm sorry, Dave. I can't do that.");
+    } else if (incrString.includes("2")) {
+      inputArray.push("Boop!");
+    } else if (incrString.includes("1")) {
+      inputArray.push("Beep!");
     } else {
-      numberList += index;
+      inputArray.push(incrString)
     }
-    if(i !== input) {
-      numList += ",";
   }
+  return inputArray
 }
-    
-
-    for (var index = 0; index < newList.length; index += 1) {
-      $("#result").prepend("<ul>" + "</ul>").append("<li>" + index + "</li>");
-    };
-    console.log(numberList)
-
-
-
-
-
-
-// Refuse non number inputs
-    if (isNaN(inputNumber)) {
-      alert("Please enter a number")
-      $("#result").hide();
-    } else
-      $("#result").show();
-    }
